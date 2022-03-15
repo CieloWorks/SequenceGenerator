1ae45a5f4039bb3016c3bd2f71da1450bfb75a62
Dean Goodman <dean@cieloworks.com>
2022-03-14 19:11:41 -0500

    Add some internal documentation
    Basic documentation about what each method in the interface should do

--- SequenceGeneratorLib/ISequenceGenerator.cs
+++ SequenceGeneratorLib/ISequenceGenerator.cs
@@ -2,7 +2,18 @@
 {
     public interface ISequenceGenerator
     {
+        /// <summary>
+        /// Returns the nth term in a numeric sequence
+        /// </summary>
+        /// <param name="n">The index of the term to be returned</param>
+        /// <returns>The nth term in a numeric sequence</returns>
         double GenerateNthTerm(int n);
+
+        /// <summary>
+        /// The sum of all terms in the sequence from 0 to the input number
+        /// </summary>
+        /// <param name="n">The number of terms from the sequence that should be added</param>
+        /// <returns>The sum of all terms in the sequence from 0 to the input number</returns>
         double SumOfTerms(int n);
     }
 }
